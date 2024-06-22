646b13f4f59103495a5db442a71f447de5496777
Ken Chase <kenchase@kenchase.com>
2024-06-22 09:31:49 -0400

    add ccpsa_update_event_status_init action

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 
 // Defines
-define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
-define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+define('FL_CHILD_THEME_DIR', get_stylesheet_directory());
+define('FL_CHILD_THEME_URL', get_stylesheet_directory_uri());
 
 // Classes
 require_once 'classes/class-fl-child-theme.php';
 
 // Actions
-add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+add_action('wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000);
 
 // Includes
 require_once get_stylesheet_directory() . '/includes/inc-custom-functions.php';
